903e0125c1cd75427788ca6c6b46f86798499b6a
Volodymyr Gerun <vgerun97@gmail.com>
2024-02-09 13:34:52 +0200

    Add Vercel Serverless Function

--- api/index.mjs
+++ api/index.mjs
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/completions", async (req, res) => {
+app.post("/api/completions", async (req, res) => {
   const options = {
     method: "POST",
     headers: {
@@ -28,7 +28,7 @@ app.post("/completions", async (req, res) => {
   try {
     const response = await fetch(
       "https://api.openai.com/v1/chat/completions",
-      options,
+      options
     );
     const data = await response.json();
     res.send(data);
@@ -38,5 +38,9 @@ app.post("/completions", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}/completions`);
+  console.log(
+    `Server API is running on http://localhost:${PORT}/api/completions`
+  );
 });
+
+export default app;
