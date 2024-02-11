41739a6130a8e5356a9a55ea3f3cc55fb2fb893d
Volodymyr Gerun <vgerun97@gmail.com>
2024-02-11 11:59:14 +0200

    Test vercel functions

--- api/hello.js
+++ api/hello.js
@@ -1,7 +1,3 @@
-// export function GET(req, res) {
-//   return new Response(`Hello from ${process.env.VERCEL_REGION}, ${req} | ${res}`);
-// }
-
-export function POST(req, res) {
-  return new Response(`POST from ${process.env.VERCEL_REGION}, ${JSON.stringify(req)} | ${JSON.stringify(res)}`);
+export function GET(req, res) {
+  return new Response(`Hello from ${process.env.VERCEL_REGION}, ${req} | ${res}`);
 }
