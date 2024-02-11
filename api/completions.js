f0a9eb0cbe922cb9c71cafa77bff036868b9be57
Volodymyr Gerun <vgerun97@gmail.com>
2024-02-11 21:46:16 +0200

    Test upstash

--- api/completions.js
+++ api/completions.js
@@ -1,8 +1,26 @@
-// import { kv } from "@vercel/kv";
-// import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "10 s"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
 
 export default async function handler(req, res) {
   try {
+    const { success } = await ratelimit.blockUntilReady("id", 10_000);
+
+    if (!success) {
+      return res.status(429).send("Too many requests");
+    }
+
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
