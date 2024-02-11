64487352212509a2e79b540f90eeb9441ae9fa5c
Volodymyr Gerun <vgerun97@gmail.com>
2024-02-11 12:30:53 +0200

    Test vercel functions

---
+++ api/post.js
@@ -0,0 +1,60 @@
+export async function POST(req, res) {
+  console.log("🚀  req:", req);
+
+  // return new Response(`Hello from ${process.env.VERCEL_REGION}, ${req} | ${res}`);
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: process.env.GPT_MODEL_NAME,
+      messages: [
+        {
+          role: "user",
+          content: req.body.message,
+        },
+      ],
+    }),
+  };
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+// app.post("/api/completions", async (req, res) => {
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       model: process.env.GPT_MODEL_NAME,
+//       messages: [
+//         {
+//           role: "user",
+//           content: req.body.message,
+//         },
+//       ],
+//     }),
+//   };
+//   try {
+//     const response = await fetch(
+//       "https://api.openai.com/v1/chat/completions",
+//       options
+//     );
+//     const data = await response.json();
+//     res.send(data);
+//   } catch (e) {
+//     console.error(e);
+//   }
+// });
