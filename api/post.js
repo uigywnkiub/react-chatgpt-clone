300a2ada5f3148fcb5dc96b54a8ff2406d17e2e6
Volodymyr Gerun <vgerun97@gmail.com>
2024-02-11 13:26:56 +0200

    Test server response

--- api/post.js
+++ api/post.js
@@ -24,7 +24,7 @@ export async function POST(req, res) {
     const data = await response.json();
     // res.setHeader("Access-Control-Allow-Credentials", `true`);
     // res.setHeader("Access-Control-Allow-Origin", "*");
-    return new Response(JSON.stringify(data));
+    return new Response(JSON.stringify(response));
     // res.send(data);
   } catch (e) {
     console.error(e);
