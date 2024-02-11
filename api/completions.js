64a6fbc03724311c53d51dfecb758ac4f9dbe30c
Volodymyr Gerun <vgerun97@gmail.com>
2024-02-11 20:43:35 +0200

    Test vercel kv

--- api/completions.js
+++ api/completions.js
@@ -1,34 +1,4 @@
-import { ipAddress, next } from '@vercel/edge'
-import { Ratelimit } from '@upstash/ratelimit'
-import { kv } from '@vercel/kv'
-
-const ratelimit = new Ratelimit({
-  redis: kv,
-  // 5 requests from the same IP in 10 seconds
-  limiter: Ratelimit.slidingWindow(2, '10 s'),
-})
-
-// Define which routes you want to rate limit
-export const config = {
-  matcher: '/completions',
-}
-
-// export default async function middleware(request) {
-//   // You could alternatively limit based on user ID or similar
-//   const ip = ipAddress(request) || '127.0.0.1'
-//   const { success, pending, limit, reset, remaining } = await ratelimit.limit(
-//     ip
-//   )
-
-//   return success ? next() : Response.redirect(new URL('/blocked.html', request.url))
-// }
-
 export default async function handler(req, res) {
-  const ip = ipAddress(request) || '127.0.0.1'
-  const { success, pending, limit, reset, remaining } = await ratelimit.limit(
-    ip
-  )
-
   try {
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
@@ -47,11 +17,8 @@ export default async function handler(req, res) {
       }),
     });
 
-    
-    // return success ? res.send(await response.json()) : Response.redirect(new URL('/blocked.html', request.url))
     return res.send(await response.json());
   } catch (error) {
     return res.status(500).send(error.message || error.toString());
   }
-  
 }
