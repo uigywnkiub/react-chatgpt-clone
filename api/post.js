538cef938aa632234f59f2e9dba9485f25a05350
Volodymyr Gerun <vgerun97@gmail.com>
2024-02-11 14:34:32 +0200

    Test server response

--- api/post.js
+++ api/post.js
@@ -1,46 +1,40 @@
-export default async function handler(req, res) {
-  console.log("🚀  req:", req.body);
+// export default async function handler(req, res) {
+//   console.log("🚀  req:", req.body);
 
-  const { body } = req;
-  return res.send(`Hello ${body.name}, you just parsed the request body!`);
-}
-// export async function POST(req, res) {
-//   const { body: reqBody } = req;
-//   if (reqBody.message === "POST") {
-//     return new Response(JSON.stringify(reqBody));
-//   }
-
-//   // return new Response(`Hello from ${process.env.VERCEL_REGION}, ${req} | ${res}`);
-//   const options = {
-//     method: "POST",
-//     headers: {
-//       Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       model: process.env.GPT_MODEL_NAME,
-//       messages: [
-//         {
-//           role: "user",
-//           content: req.body.message || "test resp",
-//         },
-//       ],
-//     }),
-//   };
-//   try {
-//     const response = await fetch(
-//       "https://api.openai.com/v1/chat/completions",
-//       options
-//     );
-//     const data = await response.json();
-//     // return new Response(JSON.stringify(data));
-//     return new Response(JSON.stringify(req.body));
-//     // return data
-//     // return res.send(data);
-//   } catch (e) {
-//     console.error(e);
-//   }
+//   const { body } = req;
+//   return res.send(`Hello ${body.name}, you just parsed the request body!`);
 // }
+
+export async function POST(req, res) {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: process.env.GPT_MODEL_NAME,
+      messages: [
+        {
+          role: "user",
+          content: req.body.message || req.body,
+        },
+      ],
+    }),
+  };
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    const data = await response.json();
+    return new Response(JSON.stringify(data));
+    // return data
+    // return res.send(data);
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 // app.post("/api/completions", async (req, res) => {
 //   const options = {
