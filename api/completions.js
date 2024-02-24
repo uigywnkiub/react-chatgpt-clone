e744412bda857ecbe0778ec9c250e49c7d40bca1
Volodymyr Gerun <vgerun97@gmail.com>
2024-02-24 16:21:38 +0200

    Revert to node js serverless runtime

--- api/completions.js
+++ api/completions.js
@@ -16,9 +16,9 @@ const ratelimit = new Ratelimit({
   prefix: "@upstash/ratelimit",
 });
 
-export const config = {
-  runtime: "edge",
-};
+// export const config = {
+//   runtime: "edge",
+// };
 
 export default async function handler(req, res) {
   try {
